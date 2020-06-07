e0bb0bb06f52e491739b7d93d46e33945c188939
Mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-06-07 15:45:34 +0530

    Add to scaleLinear

--- index.js
+++ index.js
@@ -12,3 +12,15 @@ var tooltip = body.append("div").
 attr("class", "tooltip").
 attr("id", "tooltip").
 style("opacity", 0);
+
+var unemployment = d3.map();
+
+var path = d3.geoPath();
+
+var x = d3.scaleLinear().
+domain([2.6, 75.1]).
+rangeRound([600, 860]);
+
+var color = d3.scaleThreshold().
+domain(d3.range(2.6, 75.1, (75.1 - 2.6) / 8)).
+range(d3.schemeGreens[9]);
