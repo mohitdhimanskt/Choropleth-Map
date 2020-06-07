6329be0e4fdac30c4ce35c052d0eafe688b9c305
Mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-06-07 15:44:33 +0530

    Add to tooltip

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ var body = d3.select("body");
 var svg = d3.select("svg"),
 width = +svg.attr("width"),
 height = +svg.attr("height");
+
+var tooltip = body.append("div").
+attr("class", "tooltip").
+attr("id", "tooltip").
+style("opacity", 0);
