1bab51e61d2218acb7b796e43bbb8d359a2678dc
Mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-06-07 15:47:07 +0530

    Add to map

--- index.js
+++ index.js
@@ -24,3 +24,16 @@ rangeRound([600, 860]);
 var color = d3.scaleThreshold().
 domain(d3.range(2.6, 75.1, (75.1 - 2.6) / 8)).
 range(d3.schemeGreens[9]);
+
+var g = svg.append("g").
+attr("class", "key").
+attr("id", "legend").
+attr("transform", "translate(0,40)");
+
+g.selectAll("rect").
+data(color.range().map(function (d) {
+  d = color.invertExtent(d);
+  if (d[0] == null) d[0] = x.domain()[0];
+  if (d[1] == null) d[1] = x.domain()[1];
+  return d;
+})).
